41f64d83df05423dbcc349c770228b81c585a3d8
“mk0417” <pengdata1@gmail.com>
2021-07-27 23:57:18 +0100

    clean

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-log-types '((comp) (comp) (comp) (comp) (comp) (comp)))
+ '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-tooltip ((t (:background "#ffeead" :foreground "black"))))
+ '(company-tooltip-annotation ((t (:background "#ffeead" :foreground "red"))))
+ '(company-tooltip-common ((t (:background "#ffeead" :foreground "black"))))
+ '(company-tooltip-selection ((t (:background "#69adc6" :foreground "white"))))
+ '(evil-goggles-paste-face ((t (:background "#5dace0"))))
+ '(evil-goggles-yank-face ((t (:background "#5dace0")))))

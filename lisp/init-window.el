3f6d372d27ec930461ff50c4428fe255851850d5
“mk0417” <pengdata1@gmail.com>
2021-10-02 14:30:49 +0100

    popper

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -2,6 +2,7 @@
 
 (straight-use-package 'winum)
 (straight-use-package 'switch-window)
+(straight-use-package 'popper)
 
 
 ;; winum
@@ -10,6 +11,34 @@
 ;; switch-window
 (setq-default switch-window-shortcut-style 'alphabet
               switch-window-timeout nil)
+
+;; popper
+(setq popper-mode-line nil)
+(setq popper-reference-buffers
+      '("\\*Messages\\*"
+        "Output\\*$"
+        "\\*Async Shell Command\\*"
+        "^\\*jupyter-"
+        help-mode
+        compilation-mode))
+(popper-mode 1)
+
+(defun p-popper-window-height (win)
+  ((fit-window-to-buffer
+    win
+    (floor (frame-height) 5))))
+
+(setq popper-window-height 'p-popper-window-height)
+
+(with-eval-after-load 'evil
+  (general-create-definer p-space-leader-def
+    :prefix "SPC"
+    :states '(normal visual))
+  (p-space-leader-def
+    "w"  '(:ignore t :which-key "window")
+    "wr" '(balance-windows :which-key "balance window")
+    ","  '(popper-toggle-latest :which-key "popper latest")
+    "'"  '(popper-cycle :which-key "popper cycle")))
 
 
 (provide 'init-window)

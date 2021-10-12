a22f3f6affa44d5f1d82f10a9a23fd326bcdad2d
“mk0417” <pengdata1@gmail.com>
2021-10-12 10:00:22 +0100

    remove popper

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -2,7 +2,6 @@
 
 (straight-use-package 'winum)
 (straight-use-package 'switch-window)
-(straight-use-package 'popper)
 
 
 ;; winum
@@ -12,40 +11,6 @@
 (setq-default switch-window-shortcut-style 'alphabet
               switch-window-timeout nil)
 
-;; popper
-(setq popper-mode-line
-      '(:eval (propertize " POP" 'face 'mode-line-emphasis)))
-
-(setq popper-reference-buffers
-      '("\\*Messages\\*"
-        "Output\\*$"
-        "\\*Warnings\\*$"
-        "\\*Python Doc\\*$"
-        "\\*Backtrace\\*$"
-        "\\*Shell Command Output\\*$"
-        "\\*Compile-Log\\*$"
-        "\\*Async Shell Command\\*"
-        "^\\*jupyter-"
-        help-mode
-        debugger-mode
-        compilation-mode))
-
-(popper-mode 1)
-
-(defun p-popper-window-height (win)
-  ((fit-window-to-buffer
-    win
-    (floor (frame-height) 5))))
-
-(setq popper-window-height 'p-popper-window-height)
-
-(add-to-list 'display-buffer-alist
-             '("\\*Python Doc\\*$"
-               (display-buffer-in-side-window)
-               (side . right)
-               (window-width 50)))
-
-
 ;; keybindings
 (with-eval-after-load 'evil
   (general-create-definer p-space-leader-def
@@ -53,9 +18,7 @@
     :states '(normal visual))
   (p-space-leader-def
     "w"  '(:ignore t :which-key "window")
-    "wr" '(balance-windows :which-key "balance window")
-    ","  '(popper-toggle-latest :which-key "popper latest")
-    "'"  '(popper-cycle :which-key "popper cycle")))
+    "wr" '(balance-windows :which-key "balance window")))
 
 
 (provide 'init-window)
